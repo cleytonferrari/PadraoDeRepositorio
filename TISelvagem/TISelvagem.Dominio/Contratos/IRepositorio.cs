068ff1c7c8b5a53c41abaab9149127550511622d
Cleyton Ferrari <cleyton@w7br.com>
2013-07-26 11:33:28 -0400

    adicionado o IRepositorio

---
+++ TISelvagem/TISelvagem.Dominio/Contratos/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TISelvagem.Dominio.Contratos
+{
+    public interface IRepositorio<T> where T : class
+    {
+        void Salvar(T entidade);
+
+        void Alterar(T entidade);
+
+        void Excluir(T entidade);
+
+        T Buscar(int id);
+
+        IEnumerable<T> BuscarTodos();
+
+        IEnumerable<T> BuscarPorFiltro(Func<T, bool> filtro);
+    }
+}
